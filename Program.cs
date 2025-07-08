3007e7dc1b6f5c946e9c8273dbf2e07ff4202d5d
Branko Milivojevic <branko.milivojevic@opcom.io>
2025-07-08 11:18:09 +0200

    Added Razor pages, that are calling API for generating sequence and verifying user input

--- Program.cs
+++ Program.cs
@@ -1,21 +1,27 @@
-using Senso.Repositories;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Register repository as singleton
-builder.Services.AddSingleton<IGameRepository, InMemoryGameRepository>();
+builder.Services.AddSingleton<Senso.Repositories.IGameRepository, Senso.Repositories.InMemoryGameRepository>();
 
 var app = builder.Build();
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Senso Game API v1");
+    c.RoutePrefix = "swagger"; 
+});
 
 app.UseHttpsRedirection();
 
+app.UseStaticFiles();
+app.UseRouting();
+
 app.MapControllers();
+app.MapRazorPages();
 
 app.Run();
